7c26fb1a9aa312fe908fd11e4e27a8074e49b089
aarree <aaron.epost@gmail.com>
2024-02-10 00:02:50 +0100

    watcher tryout

--- lib/defineStore/defineProxyStore.ts
+++ lib/defineStore/defineProxyStore.ts
@@ -1,18 +1,28 @@
 ﻿import {DefineStoreResponse, StoreDefinition} from "../types/StoreDefiniton.ts";
 import RootStore from "../rootStore";
-import {setupStore, SetupStoreClass} from "../store.ts";
-
+import {setupStore} from "../store.ts";
+const watchMap= new Map<string, () => void>();
 const handler = (onUpdate, name) => {
     return {
         get: function (obj, prop) {
+            console.log(prop, obj[prop]);
+            if(prop !== "getters" && prop !== "watch" && prop !== "init" && prop !== 'toJSON' ) {
+                Object.defineProperty(obj[prop],'watch', (cb) => {
+                    watchMap.set(name, cb);
+                })
+            }
+           
             if (typeof obj[prop] === "object" && obj[prop] !== null) {
-                return new Proxy(obj[prop], handler(onUpdate));
+                return new Proxy(obj[prop], handler(onUpdate, name+'.'+prop));
             }
             return obj[prop];
         },
         set: function (obj, prop, value) {
             console.log('setting prop: ', prop, 'value: ', value);
+            
             if (obj[prop] === value) return true;
+            watchMap.get(name)?.(value,obj[prop]);
+
             obj[prop] = value;
             onUpdate && onUpdate();
             return true;
